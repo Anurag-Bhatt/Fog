ac0845d44c1e4be1278b2fbb60837ffc7cfce771
Anurag-Bhatt <anuragbhatt865@gmail.com>
2025-03-24 18:48:11 +0530

    Interactable grid, changed color to red when clicked on it

--- main.cpp
+++ main.cpp
@@ -1,20 +1,28 @@
-#include <SDL3/SDL.h>
 #include <SDL3/SDL_events.h>
+#include <SDL3/SDL.h>
 #include <iostream>
 #include <vector>
-
+#include <stack>
+#include <utility>
 
 /*
     TODOS:
 
         - Make SDL windows -- done with a box
-        - Make a grid
+        - Make a grid -- done
+
+        - Make better grid walls, currentyl walls are just padding
+            but we need to connect cells incase they are 
+
         - Make that grid interactable
+            - alternates between color when a cell is clicked
+            - starting points and ending point?
+            - what else?
 
 */
 
-const int screenWidth = 640;
-const int screenHeight = 480;
+const int screenWidth = 1920;
+const int screenHeight = 1080;
 
 struct Cell{
 
@@ -66,19 +74,21 @@ int main(int argc, char* argv[]) {
     int orderOfGrid = 20;
 
     // 20 * 20 grid
-    int cellWidth   = screenWidth/orderOfGrid;
-    int cellHeight  = screenHeight/orderOfGrid;
+    int cellWidth   = 640/orderOfGrid;
+    int cellHeight  = 480/orderOfGrid;
 
     int horizontalPadding   = 5;
     int verticalPadding     = 5;
 
     // Grid of cells
     std::vector<Cell> Grid;
+    std::stack<Cell> gridStack;
+    std::vector<std::pair<int,int>> visitedCells;
 
-    for(int i = 0; i < 20; i++){
-        for(int j = 0; j < 20; j++){
+    for(int i = 0; i < orderOfGrid; i++){
+        for(int j = 0; j < orderOfGrid; j++){
             
-            SDL_Color cellColor = {.r = 0, .g = 255, .b = 0, .a = 255}; // GREEN
+            SDL_Color cellColor = {.r = 255, .g = 255, .b = 255, .a = 255}; // GREEN
     
             Grid.push_back({i * (cellWidth + horizontalPadding), 
                             j * (cellHeight + verticalPadding), 
@@ -101,7 +111,37 @@ int main(int argc, char* argv[]) {
                     quit = true;
                 }
             }
+
+            float mouseX, mouseY;
+            if(e.type == SDL_EVENT_MOUSE_MOTION){
+                
+                SDL_GetMouseState(&mouseX, &mouseY);
+                // std::cout << "Mouse Position: " << mouseX << ", " << mouseY << "\n";
+
+            }
+
+
+            if(e.type == SDL_EVENT_MOUSE_BUTTON_DOWN){
+                
+                SDL_GetMouseState(&mouseX, &mouseY);
+                if(e.button.button == SDL_BUTTON_LEFT){
+
+                    int mX = (int)mouseX/(cellWidth + horizontalPadding);
+                    int mY = (int)mouseY/(cellHeight + verticalPadding);
+
+                    std::cout << "Cell Position: " << mX << ", " << mY << "\n";
+
+                    std::cout << "Mouse button left pressed\n";
+                    if((mX < 20 && mX > 0) && (mY < 20 && mY > 0)){
+                    Grid[mY + (mX * 20)].color = {.r = 255, .g = 0, .b = 0, .a = 0};
+                    }
+                }
         }
+
+
+
+        }
+
 
         SDL_SetRenderDrawColor(ren, 0, 0, 0, 255); // Set render draw color to black
         SDL_RenderClear(ren); // Clear the renderer
